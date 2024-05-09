53016fa4d877b576aad8647d7fa5b1b6a457923b
krzysztoffila <umowy.fila@gmail.com>
2024-05-09 09:03:16 +0200

    Creating ProductList Component with simple data, styling

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,11 +1,13 @@
+import ProductList from "../Components/ProductList/ProductList";
 
 
 const Shop = () => {
     return (
         <>
             <h1>Shop</h1>
+            <ProductList />
         </>
     )
-}       
+}
 
 export default Shop;
