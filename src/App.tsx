b553b2a531eef4afda68ea2ce5306de276284041
krzysztoffila <umowy.fila@gmail.com>
2024-05-09 08:41:53 +0200

    Installed react-roputer-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom' 
-import Cart from './pages/Cart' 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Cart from './pages/Cart'
 import Favorites from './pages/Favorites'
 import Main from './pages/Main'
 import Shop from './pages/Shop'
@@ -9,16 +9,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
 
   return (
-   <>
-<BrowserRouter>
-<Routes>
-  <Route index element={<Main />} />
-  <Route path="/shop" element={< Shop />} />
-  <Route path="/cart" element={< Cart />} />
-  <Route path="/favorites" element={< Favorites />} /> 
-</Routes>
-</BrowserRouter>
-   </>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="/shop" element={< Shop />} />
+          <Route path="/cart" element={< Cart />} />
+          <Route path="/favorites" element={< Favorites />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
