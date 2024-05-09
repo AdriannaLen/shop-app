5cceb0ee8d99f6c970ae3a11adc7c9c5f4826b9f
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-05-09 11:51:57 +0200

    added logo header

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,33 +1,44 @@
 
-import { useState } from'react' 
+// import { useState } from'react' 
 import './Navigation.scss'
 
 
 const Navigation = () => {
 
-    const [isActive, setIsActive] = useState(false);
+    // below is for hamburger menu
+    // const [isActive, setIsActive] = useState(false);
 
-    const handleClick = () => {
-        setIsActive(!isActive);
-    };
+    // const handleClick = () => {
+    //     setIsActive(!isActive);
+    // };
 
     return (
 
         <>
-        <nav className={`site__nav ${isActive ? 'site__nav--active' : ''}`}>
+        {/* <nav className={`site__nav ${isActive ? 'site__nav--active' : ''}`}>
         <button className="hamburger" onClick={handleClick}>
             <span className={`hamburger__box ${isActive ? 'hamburger__box--active' : ''}`}>
                 <span className="hamburger__inner"></span>
             </span>
         </button>
-        <div className={`navigation ${isActive ? 'navigation--active' : ''}`}>
+        <div className={`navigation ${isActive ? 'navigation--active' : ''}`}> */}
+        <nav className="navigation">
+            
             <ul className="navigation__list">
                 <li className="navigation__item"><a href="/main">Main</a></li>
                 <li className="navigation__item"><a href="/shop">Shop</a></li>
                 <li className="navigation__item"><a href="/cart">Cart</a></li>
                 <li className="navigation__item"><a href="/favorites">Favorites</a></li>
             </ul>
-        </div>
+
+            <div className="logo">
+
+                <p className="logo__header">
+                    Shop in style
+                </p>
+
+            </div>
+    
         </nav>
     </>
         
