fe941e87ce77e7a4ae64c4ccedad636feaa4f42f
krzysztoffila <umowy.fila@gmail.com>
2024-05-12 20:32:41 +0200

    Fetch products from firebase to app

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Product } from '../types';
+import "./ProductCard.scss"
 
 interface ProductCardProps {
   product: Product;
@@ -7,17 +8,15 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <li className='product-list-item'>
-      <div className="card">
-        <img className="card-img-top" src={product.thumbnail} alt={product.title} />
-        <div className="card-body">
-          <h5 className="card-title">{product.title}</h5>
-          <p className="card-text">{product.description}</p>
-          <p className="card-text">Price: ${product.price}</p>
-          <button className="btn btn-primary">Add to Cart</button>
-        </div>
+    <div className="card">
+      <img className="card-img-top" src={product.thumbnail} alt={product.title} />
+      <div className="card-body">
+        <h5 className="card-title">{product.title}</h5>
+        <p className="card-text">{product.description}</p>
+        <p className="card-text">Price: ${product.price}</p>
+        <button className="btn btn-primary">Add to Cart</button>
       </div>
-    </li>
+    </div>
   );
 };
 
--- src/Components/ProductList/ProductList.tsx
+++ src/Components/ProductList/ProductList.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import ProductCard from '../ProductCard/ProductCard';
+import { useState, useEffect } from "react";
 import { Product } from '../types';
+import './ProductList.scss'
 
 interface ProductListProps {
   products: Product[];
 }
 
-const ProductList: React.FC<ProductListProps> = ({ products }) => {
+const ProductList: React.FC<ProductListProps> = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://shop-react-c0b01-default-rtdb.firebaseio.com/products/-NxNDxEEA4HYwG6BEhG0/products.json");
+      if (!response.ok) throw new Error("Something goes wrong!");
+
+      const products = await response.json();
+      setProducts(products);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Product List</h1>
       <ul className='product-list-container'>
-        {products && products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+        {products.map((product) => (
+          <li className='product-list-item' key={product.id}>
+            <ProductCard key={product.id} product={product} />
+          </li>
         ))}
       </ul>
     </>
