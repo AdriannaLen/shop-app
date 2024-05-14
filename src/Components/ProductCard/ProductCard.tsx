7e5fc18bedc6141738d201105dfbbed3069b0314
krzysztoffila <umowy.fila@gmail.com>
2024-05-14 16:25:35 +0200

    Styling cards

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -13,8 +13,10 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       <div className="card-body">
         <h5 className="card-title">{product.title}</h5>
         <p className="card-text">{product.description}</p>
-        <p className="card-text">Price: ${product.price}</p>
-        <button className="btn btn-primary">Add to Cart</button>
+        <div className='card-summary'>
+          <p className="card-text">Price: ${product.price}</p>
+          <button className="btn btn-primary">Add to Cart</button>
+        </div>
       </div>
     </div>
   );
