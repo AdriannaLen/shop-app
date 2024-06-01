7be9a2077860d595b788f015db17b16b9d4b1ef1
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-06-01 16:42:47 +0200

    added setCount to view number of selected product

--- src/Components/ShoppingCart/ShoppingCart.tsx
+++ src/Components/ShoppingCart/ShoppingCart.tsx
@@ -1,12 +1,15 @@
 import { Product } from '../types';
 import './ShoppingCart.scss'
-
+import { useState } from "react";
 interface ShoppingCartProps {
     cartItems: Product[];
     handleDelete: (id: number) => void;
   }
   
   const ShoppingCart: React.FC<ShoppingCartProps> = ({ cartItems, handleDelete }) => {
+
+    const [count, setCount] = useState(0);
+
     return (
     
 
@@ -15,6 +18,9 @@ interface ShoppingCartProps {
           {cartItems.map((item, index) => (
             <>
             <li className="buttonList" key={index}>{item.title} - ${item.price}
+            <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+          <p>{count}</p>
+          <button onClick={() => setCount((prev) => prev + 1)}>+</button>
             <button className="buttonList__delete" onClick={() => handleDelete(item.id)}>Delete</button>
             
             </li>
