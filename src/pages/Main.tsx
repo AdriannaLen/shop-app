32125783eed09903d7a8540ba05ed94b6bfa2731
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-06-03 13:42:48 +0200

    added carousel and added height: 100vh to ShoppingCart

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header"; 
 import Navigation from "../Components/Navigation/Navigation";
-import MainSection from "../Components/MainSection/MainSection";
+import ControlledCarousel from "../Components/ControlledCarouselCarousel/ControlledCarousel";
 
 
 const Main =() => {
@@ -9,7 +9,7 @@ const Main =() => {
     <>
       <Navigation />
       <Header> Shop in Style </Header>
-      <MainSection />
+      <ControlledCarousel />
       <Footer />
     
     </>
